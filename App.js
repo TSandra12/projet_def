336971e9ded01a38be278b8559e7b02187209a28
TSandra12 <120740821+TSandra12@users.noreply.github.com>
2023-06-08 00:38:23 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,77 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-const PomodoroApp = () => {
-  const [timer, setTimer] = useState(25 * 60); // Durée de travail en secondes
-  const [breakTime, setBreakTime] = useState(5 * 60); // Durée de pause en secondes
-  const [isRunning, setIsRunning] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    let intervalId;
-
-    if (isRunning) {
-      intervalId = setInterval(() => {
-        if (timer > 0) {
-          setTimer((prevTimer) => prevTimer - 1);
-        } else {
-          handleCompleteTimer();
-        }
-      }, 1000);
-    }
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [isRunning, timer]);
-
-  const handleStartTimer = () => {
-    setIsRunning(true);
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
   };
 
-  const handlePauseTimer = () => {
-    setIsRunning(false);
-  };
-
-  const handleResetTimer = () => {
-    setTimer(25 * 60);
-    setBreakTime(5 * 60);
-    setIsRunning(false);
-    setIsBreak(false);
-  };
-
-  const handleCompleteTimer = () => {
-    if (isBreak) {
-      setTimer(25 * 60);
-      setIsBreak(false);
-    } else {
-      setTimer(breakTime);
-      setIsBreak(true);
-    }
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
   };
 
   return (
     <div>
-      <h1>Pomodoro Sandra</h1>
-      <div>
-        <h2>{isBreak ? "Break Time" : "Work Time"}</h2>
-        <h3>{formatTime(timer)}</h3>
-        {isRunning ? (
-          <button onClick={handlePauseTimer}>Pause</button>
-        ) : (
-          <button onClick={handleStartTimer}>début</button>
-        )}
-        <button onClick={handleResetTimer}>recommencer</button>
-      </div>
+      <h1>compteur</h1>
+      <h2>N°: {count}</h2>
+      <button onClick={increment}>incrementation</button>
+      <button onClick={decrement}>decrementation</button>
     </div>
   );
 };
 
-export default PomodoroApp;
+export default CounterApp;
